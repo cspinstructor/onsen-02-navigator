30f233c2054f35824a8688a6edaaa8ef895408e5
paulchin <paulchin20@yahoo.com>
2019-04-14 22:45:36 +0800

    'update'

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -1,11 +1,33 @@
+//--- Fires first ---
 $(document).on('init', function(event) {
-  //ons.notification.alert('Button is tapped!');
   var page = event.target;
   if (page.id === 'page1') {
     $('#push-button').on('click', function() {
-      $('#myNavigator')[0].pushPage('page2.html', {
-        data: { title: 'Page 2' }
-      });
+      $('#myNavigator')[0]
+        .pushPage('page2.html', {
+          data: { message: 'it works' }
+        })
+        .then(function() {
+          ons.notification.alert('page pushed');
+        });
+    });
+  }
+
+  if (page.id === 'page2') {
+    $('#push-button-2').on('click', function() {
+      ons.notification.alert('page2 button pushed');
     });
   }
 });
+
+//--- Fires second after document init above ---
+ons.ready(function() {
+  ons.notification.alert('i am ready');
+});
+
+//--- called directly from page2.html div inline ---
+//--- receives the data passed by navigator pushPage ---
+function getMessage() {
+  var msg = $('#myNavigator')[0].topPage.data.message;
+  $('#pageMessage').html(msg);
+}

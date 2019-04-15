4530b07029e0c97823565103eeda71d06c31f975
paulchin <paulchin20@yahoo.com>
2019-04-15 18:03:33 +0800

    'u'

--- www/app.js
+++ www/app.js
@@ -8,7 +8,7 @@ $(document).on('init', function(event) {
           data: { message: 'it works' }
         })
         .then(function() {
-          ons.notification.alert('page pushed');
+          //ons.notification.alert('page pushed');
         });
     });
   }
@@ -23,6 +23,9 @@ $(document).on('init', function(event) {
 //--- Fires second after document init above ---
 ons.ready(function() {
   ons.notification.alert('i am ready');
+  changeAnimation();
+  getTopPage();
+  //pushApage();
 });
 
 //--- called directly from page2.html div inline ---
@@ -31,3 +34,30 @@ function getMessage() {
   var msg = $('#myNavigator')[0].topPage.data.message;
   $('#pageMessage').html(msg);
 }
+
+//--- changes an attribute of the navigator ---
+function changeAnimation() {
+  $('#myNavigator').attr('animation', 'lift');
+}
+
+//--- gets a property of the navigator ---
+function getTopPage() {
+  var myNavigator = $('ons-navigator')[0];
+  console.log(myNavigator.topPage);
+}
+
+//--- calls a method of the navigator ---
+function pushApage() {
+  var myNavigator = $('ons-navigator')[0];
+  myNavigator.pushPage('page2.html');
+}
+
+//--- listen to an event of the navigator ---
+var myNavigator = $('ons-navigator');
+myNavigator.on('postpush', function(event) {
+  console.log('pushPage is completed: ', event);
+});
+
+// myNavigator.addEventListener('postpush', function(event) {
+//   console.log('pushPage is completed: ', event);
+// });
